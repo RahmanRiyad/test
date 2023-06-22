80b70c0171f7407dc8b3cf80d1c96bd9e220c9cb
Naiem Ur Rahman Riyad <137284605+RahmanRiyad@users.noreply.github.com>
2023-06-22 12:14:41 +0600

    Update test.java

--- test.java
+++ test.java
@@ -1,7 +1,7 @@
 public class test{
   public static void main(String[] args) {
     System.out.println("Hi I am Ryzen");
-    System.out.printf("Nothing is pleasent");
+    System.out.println("Nothing is pleasent");
     System.out.println("Just a beginning but never stop learning");
 System.out.println("I am on my way");
  System.out.println("Nobody want's you do better");
