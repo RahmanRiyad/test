2505b22c11647e61f9c14a220ff52720ea983ded
Naiem Ur Rahman Riyad <137284605+RahmanRiyad@users.noreply.github.com>
2023-06-22 12:06:54 +0600

    Update test.java

--- test.java
+++ test.java
@@ -2,5 +2,6 @@ public class test{
   public static void main(String[] args) {
     System.out.println("Hi I am Ryzen");
     System.out.printf("Nothing is pleasent");
+    System.out.println("Just a beginning but never stop learning");
   }
 }
